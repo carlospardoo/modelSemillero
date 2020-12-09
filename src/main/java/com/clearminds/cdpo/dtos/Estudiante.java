595884437ac4d81c776409aefd906fc4c63c88e2
Carlos Pardo <28719833+carlospardoo@users.noreply.github.com>
2020-12-09 11:54:19 -0500

    Corregido getters y setters de Estudiante

--- src/main/java/com/clearminds/cdpo/dtos/Estudiante.java
+++ src/main/java/com/clearminds/cdpo/dtos/Estudiante.java
@@ -18,6 +18,30 @@ public class Estudiante {
 		this.apellido = apellido;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
 	@Override
 	public String toString() {
 		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", numero=" + numero + "]";
